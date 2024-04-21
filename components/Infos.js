b9acd53c238069f587bed120e368d04ad227a861
ARTHURcantu <cantuario.arthur@gmail.com>
2024-04-20 21:05:32 -0300

    build(construindo a base do aplicativo)

---
+++ components/Infos.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Info = () => {
+    <>
+        <View style={styles.container}>
+        <Text>Olá Mundo!!!!</Text>
+        <StatusBar style="auto" />
+        </View>
+    </>
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default Info
