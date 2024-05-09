33ceb47a0d1298ec44bce738b7cdef28df319f3d
Bryan-Machado <bryan.machado.soares@gmail.com>
2024-05-09 17:04:04 -0300

    redesign screen lixo

--- screens/Lixo.js
+++ screens/Lixo.js
@@ -4,35 +4,28 @@ import { View, Text, StyleSheet, Image, ScrollView } from "react-native"
 const Lixo = () => {
     return (
         <ScrollView contentContainerStyle={styles.container}>
-            <View style={styles.row}>
-                <View style={styles.column}>
-                    <Image 
-                        source={require('../assets/images/remedios.jpeg')}
-                        resizeMode="center"
-                        style={styles.image}
-                    />
-                </View>
-                <View style={styles.column2}>
-                    <Text style={styles.title}>Titulo do Lixo</Text>
-                    <Text style={styles.paragraph}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus in risus ac auctor. Aliquam aliquam eleifend tortor non iaculis. Praesent magna purus, porta quis ultrices vel, hendrerit et ipsum. In placerat convallis risus, vitae egestas libero pretium eu. Integer sed faucibus ipsum. Donec tristique ipsum nunc, ac ultrices urna semper at. Fusce eget euismod magna. Integer eleifend dolor justo, sit amet molestie sem tristique nec. Vivamus vitae euismod velit. Cras auctor elit quis ornare ullamcorper.
-                    </Text>
-                </View>
+            <View style={styles.imageView}>
+                <Image
+                    source={require('../assets/images/remedios.jpeg')}
+                    resizeMode="stretch"
+                    style={styles.image}
+                />
+                <Text style={styles.title}>Pílulas</Text>
             </View>
-
             <View>
-                <Text>
+                <Text style={styles.paragraph}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean luctus in risus ac auctor. Aliquam aliquam eleifend tortor non iaculis. Praesent magna purus, porta quis ultrices vel, hendrerit et ipsum. In placerat convallis risus, vitae egestas libero pretium eu. Integer sed faucibus ipsum. Donec tristique ipsum nunc, ac ultrices urna semper at. Fusce eget euismod magna. Integer eleifend dolor justo, sit amet molestie sem tristique nec. Vivamus vitae euismod velit. Cras auctor elit quis ornare ullamcorper.
                 </Text>
-                <View>
-                    <Text>Titulo: onde descarta</Text>
-                    <View>
-                        <Text>imagem circular 1</Text>
-                        <Text>imagem circular 2</Text>
-                        <Text>imagem circular 3</Text>
-                        <Text>imagem circular 4</Text>
-                        <Text>imagem circular 5</Text>
-                    </View>
+            </View>
+
+
+            <View style={styles.discardCard}>
+                <Text style={styles.discardTitle}>Titulo: onde descarta</Text>
+                <View style={styles.row}>
+                    <Image source={require('../assets/images/icon50x50.jpg')} style={styles.trashIcon}/>
+                    <Image source={require('../assets/images/icon50x50.jpg')} style={styles.trashIcon}/>
+                    <Image source={require('../assets/images/icon50x50.jpg')} style={styles.trashIcon}/>
+                    <Image source={require('../assets/images/icon50x50.jpg')} style={styles.trashIcon}/>
                 </View>
             </View>
 
@@ -44,13 +37,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center'
-    },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        backgroundColor: '#d8f',
-        padding: 20
     },
     column: {
         flex: 1,
@@ -66,18 +52,57 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         paddingHorizontal: 10
     },
+    imageView: {
+        backgroundColor: '#f0a',
+        width: '100%',
+        marginBottom: 100
+    },
     image: {
         flex: 1,
-        overflow: 'hidden'
+        maxHeight: 200,
+        width: '100%'
     },
     title: {
         fontSize: 24,
-        marginBottom: 15,
         fontWeight: 'bold',
-        color: '#000'
+        color: '#000',
+        backgroundColor: '#fff',
+        paddingVertical: 10,
+        paddingHorizontal: '20%',
+        position: 'absolute',
+        bottom: -25,
+        alignSelf: 'center',
+        borderRadius: 10
     },
     paragraph: {
-        color: '#000'
+        color: '#000',
+        width: '90%',
+        alignSelf: 'center',
+        fontWeight: '400',
+        fontSize: 18,
+        marginBottom: 50
+    },
+    discardCard: {
+        width: '100%',
+        paddingBottom: 20
+    },
+    discardTitle: {
+        alignSelf: 'center',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%',
+        backgroundColor: '#d8f',
+        marginTop: 20
+    },
+    trashIcon: {
+        width: 60,
+        height: 60,
+        borderRadius: '50%'
     }
 })
 
